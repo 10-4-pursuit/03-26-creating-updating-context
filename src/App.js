a3e0003fdace17fef9eb8431067452fb7e0abaa5
KyronGreen6 <kyrongreen@pursuit.org>
2024-03-11 22:13:18 -0400

    Completed Creating and Updating Context exercise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import UserContext from './UserContext';
+import UserProfile from './UserProfile';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState({ name: 'John Doe', age: 30})
+  const contextValue = { user, setUser };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserContext.Provider value = { contextValue }>
+        <UserProfile/>
+      </UserContext.Provider>
     </div>
   );
 }
